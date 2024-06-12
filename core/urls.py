c9cd2d087c278942e6ac1b8196775d3d0c3c5daa
SergioNascimento07 <sergioricjr7@gmail.com>
2024-06-12 20:57:48 -0300

    chore: urls de usuário adicionadas ao projeto

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from user.views import UserDeleteView, UserInsert, UserLogin, UserUpdate
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("user_insert", UserInsert.as_view(), name="User Insert"),
+    path("user/delete/", UserDeleteView.as_view(), name="User Delete View"),
+    path("user_update/<pk>", UserUpdate.as_view(), name="User Update"),
+    path("login", UserLogin.as_view(), name="Login"),
 ]
