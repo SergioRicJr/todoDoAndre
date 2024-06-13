3e6c54d1b9c479104b416701be7b1db317b80b27
SergioNascimento07 <sergioricjr7@gmail.com>
2024-06-12 22:25:26 -0300

    chore: urls de tasks adicionadas ao urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
+from todo.views import TaskDeleteView, TaskInsert, TaskUpdate, TaskView
 from user.views import UserDeleteView, UserInsert, UserLogin, UserUpdate
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TaskView.as_view(), name="Task View"),
+    path("insert", TaskInsert.as_view(), name="Task Insert"),
+    path("weather/delete/", TaskDeleteView.as_view(), name="delete_task"),
+    path("update/<pk>", TaskUpdate.as_view(), name="Task Update"),
     path("user_insert", UserInsert.as_view(), name="User Insert"),
     path("user/delete/", UserDeleteView.as_view(), name="User Delete View"),
     path("user_update/<pk>", UserUpdate.as_view(), name="User Update"),
