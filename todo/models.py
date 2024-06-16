7df5133575ae4a8aaf133ed2451cf0f60d9d047a
Sergio <sergioricjr7@gmail.com>
2024-06-16 02:06:43 -0300

    feat: task model e serializer alterados para conter user_id da task

--- todo/models.py
+++ todo/models.py
@@ -2,11 +2,12 @@ from typing import Any
 from datetime import datetime
 
 class TaskEntity:
-    def __init__(self, name: str, description: str, due_date: datetime, completed: bool = False) -> None:
+    def __init__(self, name: str, description: str, due_date: datetime, user_id, completed: bool = False) -> None:
         self.name = name
         self.description = description
         self.due_date = due_date
         self.completed = completed
+        self.user_id = user_id
 
     def __str__(self) -> str:
         return f"Task <{self.name}>"
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 from .models import TaskEntity
 
+
 class TaskSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField()
     due_date = serializers.DateTimeField()
     completed = serializers.BooleanField(default=False)
+    user_id = serializers.CharField(max_length=255)
 
     def create(self, validated_data):
         return TaskEntity(**validated_data)
+
+
+class TaskUpdateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255, required=False)
+    description = serializers.CharField(required=False)
+    due_date = serializers.DateTimeField(required=False)
+    completed = serializers.BooleanField(required=False)
+
+    # def create(self, validated_data):
+    #     return TaskEntity(**validated_data)
